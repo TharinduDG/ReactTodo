3639a600e153b28a0d25d5713ff1ab6247588cae
TharinduDG <tdgalappaththi@gmail.com>
2018-04-04 21:27:34 +0530

    add styles

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,8 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var TodoApp = require('TodoApp');
 
 // Load foundation
+require('style-loader!css!foundation-sites/dist/css/foundation.min.css');
+require('style-loader!css-loader!foundation-sites/dist/css/foundation-float.min.css');
 $(document).foundation();
 
 require('style!css!sass!ApplicationStyles');
